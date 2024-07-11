faefc282d44c6a29ef7e7e3849595f8c25b227a3
SamuelArdi <149386215+SamuelArdi@users.noreply.github.com>
2024-07-11 23:21:41 +0800

    used the prettier extension

--- improved-main.cpp
+++ improved-main.cpp
@@ -5,47 +5,52 @@
 using std::cout;
 using std::string;
 
-double calculator(char op, double num1, double num2) {
+double calculator(char op, double num1, double num2)
+{
     double result;
-    switch (op) {
-        case '+':
-            result = num1 + num2;
-            break;
-        
-        case '-':
-            result = num1 - num2;
-            break;
-        
-        case '*':
-            result = num1 * num2;
-            break;
+    switch (op)
+    {
+    case '+':
+        result = num1 + num2;
+        break;
 
-        case '/':
-            result = num1 / num2;
-            break;
+    case '-':
+        result = num1 - num2;
+        break;
 
-        default:
-            break;
+    case '*':
+        result = num1 * num2;
+        break;
+
+    case '/':
+        result = num1 / num2;
+        break;
+
+    default:
+        break;
     }
-    
+
     return result;
 }
 
-int main() {
+int main()
+{
     cout << "--------------- * Avterion Calculator * ---------------\n";
     cout << "Currently available operators:\n";
     cout << "Addition (+)\n";
     cout << "Subtraction (-)\n";
     cout << "Multiplication (*)\n";
     cout << "Division (/)\n";
-    
+
     bool active = true;
-    while (active == true) {
+    while (active == true)
+    {
         string input;
         cout << "> ";
         getline(std::cin, input);
 
-        if (input == "exit") {
+        if (input == "exit")
+        {
             cout << "Exiting...";
             break;
         }
@@ -54,10 +59,12 @@ int main() {
         string op;
         string ops[] = {"+", "-", "*", "/"}; // the available operators
         bool op_found = false;
-        for (int i = 0; i < std::end(ops) - std::begin(ops); i++) {
+        for (int i = 0; i < std::end(ops) - std::begin(ops); i++)
+        {
             string token;
             token = ops[i];
-            if (input.find(token) != string::npos) {
+            if (input.find(token) != string::npos)
+            {
                 op = token;
                 op_found = true;
                 break;
@@ -66,23 +73,27 @@ int main() {
 
         double num1;
         double num2;
-        try {
+        try
+        {
             string num1_str = input.substr(0, input.find_first_of(op));
             num1 = std::stod(num1_str);
 
             string num2_str = input.substr(input.find_last_of(op) + 1);
             num2 = std::stod(num2_str);
-            
-            if (op_found == true) {
+
+            if (op_found == true)
+            {
                 char op_c = static_cast<char>(op[0]);
                 double result = calculator(op_c, num1, num2);
                 cout << "Result: " << result << std::endl;
             }
-            else {
+            else
+            {
                 cout << "Requested operator is not available\n";
             }
         }
-        catch (const std::exception& e) {
+        catch (const std::exception &e)
+        {
             cout << "Please input a number\n";
         }
     }
